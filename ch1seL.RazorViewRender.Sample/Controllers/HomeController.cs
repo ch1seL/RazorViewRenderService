03f1f8fdafbfbef101bd60b43eb647f0b1e3541c
Alexander Salamatov <asalamatov@appulate.com>
2020-01-10 17:05:02 +0300

    move to ch1seL.RazorViewRender

---
+++ ch1seL.RazorViewRender.Sample/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using ch1seL.RazorViewRender;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RazorViewRender.Sample.Models;
+
+namespace RazorViewRender.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly RazorViewRenderService _razorViewRenderService;
+
+        public HomeController(ILogger<HomeController> logger, RazorViewRenderService razorViewRenderService)
+        {
+            _logger = logger;
+            _razorViewRenderService = razorViewRenderService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+
+        public async Task<IActionResult> FooBar()
+        {
+            var content = await _razorViewRenderService.RenderStringAsync("Sample/Template",
+                new FooBarModel
+                {
+                    Foo = "Foo",
+                    Bar = "<b>B</b>ar"
+                });
+
+            return new ContentResult
+            {
+                Content = content,
+                ContentType = "text/html",
+                StatusCode = 200
+            };
+        }
+    }
+}
---
+++ ch1seL.RazorViewRender/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ch1seL.RazorViewRender
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRazorViewRenderService(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection.AddScoped<RazorViewRenderService>();
+        }
+    }
+}
